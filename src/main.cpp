30c730b93a7ed25b2b0435bd676c40a00392a2b6
Martin Angelo De Guzman <mdeg011@xe-01.cs.ucr.edu>
2024-02-01 21:30:48 -0800

    Finshed implementing finding p q phi(n) and d. Still working on decryption

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -14,8 +16,6 @@ long gcd(long a, long b) {
     return a;
 }
 
-
-
 bool is_valid(long e, long n){
     if(e < 2 || e >= n){
         return false;
@@ -27,7 +27,51 @@ bool is_valid(long e, long n){
     return true;
 }
 
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+std::vector<int> findPrimeFactors(int n) {
+    std::vector<int> primeFactors;
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0 && isPrime(i) && isPrime(n/i) && i != (n/i)) { //make sure both factors are prime and are not equal to each other
+            primeFactors.push_back(i);  
+            primeFactors.push_back(n/i);                                       //get second prime number by diving first by n
+            break;                                                                 // We only need two prime factors
+        }
+    }
+
+    return primeFactors;
+}
+
+long long modInverse(long long e, long long phiN) {
+    long long d = 0;
+    while ((e * d) % phiN != 1) { //continue incrementing d until the product is congruent to 1 mod phi of n
+        d++;
+    }
+    return d;
+}
+
+// int decrypt(int c, int d, int n) {
+//     int answer = 1;
+//     c = c % n;
+//     while (d > 0) {
+//         if (d % 2 == 1) {
+//             answer = (answer*c) % n;
+//         }
+//         c = (c * c) % n;
+//         d /= 2;
+//     }
+//     return answer;
+// }
 
 int main()
 {   
@@ -39,36 +83,42 @@ int main()
     // int phiN = 4320;
     // int d = 3703;
     // int m = 0;
-    int e, n, m;
-    string c = "";
+    int e, n, m, p, q, phiN, d;
+    int c;
+    long long decrypted;
+    
 
-
+    //Part (i)
     cout << "Enter e: " << endl;
     cin >> e;
     cout << "Enter n: " << endl;
     cin >> n;
-
-
+    //Part (ii)
     if(is_valid(e,n)){
         cout << "The public key you have entered is VALID!" << endl;
     }
     else{
         cout << "INVALID PUBLIC KEY!!!" << endl;
+        return 0;                               //quit if invalid
     }
     
-
-
     cout << "Enter m (the number of characters in the message): " << endl;
     cin >> m;
-
-    cout << "Enter ciphertext (c): ";
+    cout << "Enter cyphertext (c): ";
     cin >> c;
 
+    //Part (vi)
+    vector<int> factorsN = findPrimeFactors(n);
+    p = factorsN[0];
+    q = factorsN[1];
+    phiN =  (p-1)*(q-1);
+    d = modInverse(e, phiN);
+    cout << "p: " << p << " and q: " << q << endl;
+    cout << "phi(n): " << phiN << endl;
+    cout << "d: " << d << endl;
 
-    // cout << "p: " << p << endl;
-    // cout << "q: " << q << endl;
-    // cout << "φ(n): " << phiN << endl;
-    // cout << "d: " << d << endl;
+    // decrypted = (c, d, n);
+    // cout << "Decrypted int: " << decrypted << endl;
     
 
     return 0;
