3d7191937f259cd122fdcd23caf215ffebddddc6
Martin Angelo De Guzman <mdeg011@xe-01.cs.ucr.edu>
2024-02-02 22:38:26 -0800

    Finished converting decrypted int to string message. Also made the whole code follow the rules on the RSA code specifications document

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <math.h>
+#include <sstream>
 
 using namespace std;
 
@@ -39,22 +40,21 @@ bool isPrime(long long n) {
     return true;
 }
 
-std::vector<long long> findPrimeFactors(long long n) {
-    std::vector<long long> primeFactors;
+vector<long long> findPrimeFactors(long long n) {
+    vector<long long> primeFactors;
     for (int i = 2; i <= n/2; i++) {
-        if (n % i == 0 && isPrime(i) && isPrime(n/i) && i != (n/i)) { //make sure both factors are prime and are not equal to each other
+        if (n % i == 0 && isPrime(i) && isPrime(n/i) && i != (n/i)) {           //make sure both factors are prime and are not equal to each other
             primeFactors.push_back(i);  
             primeFactors.push_back(n/i);                                       //get second prime number by diving first by n
-            break;                                                                 // We only need two prime factors
+            break;                                                             // We only need two prime factors
         }
     }
-
     return primeFactors;
 }
 
 long long modInverse(long long e, long long phiN) {
     long long d = 0;
-    while ((e * d) % phiN != 1) { //continue incrementing d until the product is congruent to 1 mod phi of n
+    while ((e * d) % phiN != 1) {                                               //continue incrementing d until the product is congruent to 1 mod phi of n
         d++;
     }
     return d;
@@ -64,56 +64,70 @@ long long modularMultiplication(long long c, long long d, long long n) {
     long long result = 0;
     c %= n;
     while (d > 0) {
-        if (d & 1) {        //check if d is odd, if so take it out and use recurrence
+        if (d & 1) {                                                        //check if d is odd, if so take it out and use recurrence
             result = (result + c) % n;
         }
         c = (2 * c) % n;
-        d >>= 1;        //shift is equivalent to dividing by two, more efficient for big numbers
+        d >>= 1;                                                            //shift is equivalent to dividing by two, more efficient for big numbers
     }
     return result;
 }
 
 long long decrypt(long long c, long long d, long long n) {           //Use recurrence recursively to make sure number doesnt get too big for large exponents
-    // Base cases
     if (d == 0)
         return 1;
     if (d == 1)
         return c % n;
-    
     long long halfPower = decrypt(c, d >> 1, n);                         //divide d by two to get 'half power' and square d instead
     long long halfPowerSquared = modularMultiplication(halfPower, halfPower, n);
-    
-    // If the exponent is odd
-    if (d & 1) {
+    if (d & 1) {                                                        //if exponent is odd, take it out to be even and run even algorithm
         return modularMultiplication(halfPowerSquared, c, n);
     }
-    
     return halfPowerSquared;
 }
 
 int main()
 {   
     long long e, n, m, p, q, phiN, d, c;
+    string cipherText;
+    vector<long long> cipherNumbers;
     
 
     //Part (i)
-    cout << "Enter e: " << endl;
-    cin >> e;
-    cout << "Enter n: " << endl;
-    cin >> n;
+    //cout << "Enter e: " << endl;
+    cin >> e >> n;
+    //cout << "Enter n: " << endl;
+    //cin >> n;
+
+
+    //cout << "Enter m (the number of characters in the message): " << endl;
+    cin >> m;\
+    cin.ignore();
+    //cout << "Enter ciphertext (c): ";
+    getline(cin, cipherText);
+
     //Part (ii)
     if(is_valid(e,n)){
         cout << "The public key you have entered is VALID!" << endl;
     }
     else{
         cout << "INVALID PUBLIC KEY!!!" << endl;
-        return 0;                               //quit if invalid
+        return 0;                                                               //quit if invalid
     }
-    
-    cout << "Enter m (the number of characters in the message): " << endl;
-    cin >> m;
-    cout << "Enter cyphertext (c): ";
-    cin >> c;
+
+    //get ciphertext and convert the string input into integer for the decrypt algorithm to decrypt
+    string token;
+    for (char c : cipherText) {
+        if (isdigit(c)) {
+            token += c;
+        } else if (!token.empty()) {
+            cipherNumbers.push_back(stoi(token));
+            token.clear();
+        }
+    }
+    if (!token.empty()) {
+        cipherNumbers.push_back(stoi(token));
+    }
 
     //Part (vi)
     vector<long long> factorsN = findPrimeFactors(n);
@@ -121,14 +135,50 @@ int main()
     q = factorsN[1];
     phiN =  (p-1)*(q-1);
     d = modInverse(e, phiN);
-    cout << "p: " << p << " and q: " << q << endl;
-    cout << "phi(n): " << phiN << endl;
-    cout << "d: " << d << endl;
+    cout  << p << " " << q << " " << phiN << " " << d << " " << endl;
+    long long decrypted; 
 
-    long long decrypted = decrypt(c, d, n);
+    for (long long cipherNumber : cipherNumbers){
+        decrypted = decrypt(cipherNumber, d, n);
+        cout << decrypted << " ";
+    }
 
-    cout << "Decrypted int: " << decrypted << endl;
-    
-
+    for (long long cipherNumber : cipherNumbers){
+        decrypted = decrypt(cipherNumber, d, n);
+        switch(decrypted){
+        case(7): cout << 'A' ; break;
+        case(8): cout << 'B' ; break;
+        case(9): cout << 'C' ; break;
+        case(10): cout << 'D' ; break;
+        case(11): cout << 'E' ; break;
+        case(12): cout << 'F' ; break;
+        case(13): cout << 'G' ; break;
+        case(14): cout << 'H' ; break;
+        case(15): cout << 'I' ; break;
+        case(16): cout << 'J' ; break;
+        case(17): cout << 'K' ; break;
+        case(18): cout << 'L' ; break;
+        case(19): cout << 'M' ; break;
+        case(20): cout << 'N' ; break;
+        case(21): cout << 'O' ; break;
+        case(22): cout << 'P' ; break;
+        case(23): cout << 'Q' ; break;
+        case(24): cout << 'R' ; break;
+        case(25): cout << 'S' ; break;
+        case(26): cout << 'T' ; break;
+        case(27): cout << 'U' ; break;
+        case(28): cout << 'V' ; break;
+        case(29): cout << 'W' ; break;
+        case(30): cout << 'X' ; break;
+        case(31): cout << 'Y' ; break;
+        case(32): cout << 'Z' ; break;
+        case(33): cout << ' ' ; break;
+        case(34): cout << '"' ; break;
+        case(35): cout << ',' ; break;
+        case(36): cout << '.' ; break;
+        case(37): cout << "'"; break;
+        }
+    }
+    cout << endl;
     return 0;
 }
